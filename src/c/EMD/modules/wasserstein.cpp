c42288a3a8077c4774af6976ff342935dccc36e6
Stefanos Dianellos <stefanosdianellos@gmail.com>
2020-12-29 17:49:03 +0200

    Bug Fix

--- src/c/EMD/modules/wasserstein.cpp
+++ src/c/EMD/modules/wasserstein.cpp
@@ -1,20 +1,19 @@
+#include <iostream>
 #include <algorithm>
 #include <numeric> // std::iota
 #include <vector>
 #include "../headers/wassersten.h"
 using namespace std;
 
-vector<size_t> argsort(const vector<int> &v) {
-    vector<size_t> idx;
+void argsort(const vector<int> &v, vector<int> &idx) {
     iota(idx.begin(), idx.end(), 0);
     // sort indexes based on values in v
-    sort(idx.begin(), idx.end(), [&v](size_t i1, size_t i2){ return v[i1] < v[i2]; });
-    return idx;
+    sort(idx.begin(), idx.end(), [&v](int i1, int i2){ return v[i1] < v[i2]; });
 }
 
 vector<int> diff(const vector<int> &v) {
     vector<int> delta;
-    for (size_t i = 0; i < v.size() - 1; ++i) {
+    for (unsigned int i = 0; i < v.size() - 1; ++i) {
         delta.push_back(v[i + 1] - v[i]);
     }
     return delta;
@@ -29,13 +28,13 @@ vector<int> concatenateSort(const vector<int> &A, const vector<int> &B) {
     return C;
 }
 
-vector<size_t> searchSorted(const vector<int> &v, const vector<size_t> &idx, const vector<int> &allValues) {
-    vector<size_t> cdfIdx;
+void searchSorted(const vector<int> &v, const vector<int> &idx, const vector<int> &allValues, vector<int> &cdfIdx) {
+    
     // for every element in all indices, find id in A
     cdfIdx.reserve(allValues.size());
     for (auto elem = allValues.begin(); elem != allValues.end() - 1; ++elem) {
         auto up = upper_bound(idx.begin(), idx.end(), *elem,
-                              [&v](int elem, size_t i) { return elem < v[i]; });
+                              [&v](int elem, int i) { return elem < v[i]; });
         if (up == idx.end()) {
             cdfIdx.push_back(idx.size());
         }
@@ -43,31 +42,30 @@ vector<size_t> searchSorted(const vector<int> &v, const vector<size_t> &idx, con
             cdfIdx.push_back(up - idx.begin());
         }
     }
-    return cdfIdx;
 }
 
-vector<int> computeCDF(const vector<int> &weights, vector<size_t> &idx, const vector<size_t> &cdfIdx) {
-    vector<int> cdf;
+void computeCDF(const vector<int> &weights, vector<int> &idx, const vector<int> &cdfIdx, vector<int> &cdf) {
     vector<int> ordW;
-    for (auto i : idx) {
+
+    for (unsigned int i = 0; i < idx.size(); i++) {
         ordW.push_back(weights[i]);
     }
 
     cdf.reserve(1 + ordW.size());
     vector<int> sortedAccW;
-    sortedAccW.push_back(0);
+    // sortedAccW.resize(0);
+    // sortedAccW.push_back(0);
     partial_sum(ordW.begin(), ordW.end(), ordW.begin());
     sortedAccW.insert(sortedAccW.end(), ordW.begin(), ordW.end());
     int accum = sortedAccW.back();
     for (auto cIdx: cdfIdx) {
         cdf.push_back(sortedAccW[cIdx] / accum);
     }
-    return cdf;
 }
 
 double computeDist(const vector<int> &cdfA, const vector<int> &cdfB, const vector<int> &deltas) {
     double dist = 0.0;
-    for (size_t i = 0; i < deltas.size(); ++i) {
+    for (unsigned int i = 0; i < deltas.size(); ++i) {
         dist += abs(cdfA[i] - cdfB[i]) * deltas[i];
     }
     return dist;
@@ -75,20 +73,20 @@ double computeDist(const vector<int> &cdfA, const vector<int> &cdfB, const vecto
 
 double wasserstein(vector<int> &A, vector<int> AWeights, vector<int> &B, vector<int> BWeights) {
     vector<int> allValues, deltas, cdfA, cdfB;
-    vector<size_t> cdfIdxA, cdfIdxB;
-    vector<size_t> idxA(A.size());
-    vector<size_t> idxB(B.size());
+    vector<int> cdfIdxA, cdfIdxB;
+    vector<int> idxA(A.size());
+    vector<int> idxB(B.size());
 
     allValues = concatenateSort(A, B);
     deltas = diff(allValues);
 
-    idxA = argsort(A);
-    idxB = argsort(B);
+    argsort(A, idxA);
+    argsort(B, idxB);
 
-    cdfIdxA = searchSorted(A, idxA, allValues);
-    cdfIdxB = searchSorted(B, idxB, allValues);
-    cdfA = computeCDF(AWeights, idxA, cdfIdxA);
-    cdfB = computeCDF(BWeights, idxB, cdfIdxB);
+    searchSorted(A, idxA, allValues, cdfIdxA);
+    searchSorted(B, idxB, allValues, cdfIdxB);
+    computeCDF(AWeights, idxA, cdfIdxA, cdfA);
+    computeCDF(BWeights, idxB, cdfIdxB, cdfB);
 
     return computeDist(cdfA, cdfB, deltas);
 }
