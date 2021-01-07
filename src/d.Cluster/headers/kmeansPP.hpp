c61cd9f2dc5d0cf042192497bf607f36e52df105
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2021-01-07 14:03:23 +0200

    wip

---
+++ src/d.Cluster/headers/kmeansPP.hpp
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <vector>
+#include <string>
+// random value for now find the best
+#define SMALL_E 5 
+#define MAX_LOOPS 50
+
+using namespace std;
+
+extern vector<pair<int, unsigned int> > nearest_clusters;
+// in the centroid vectors, store the indexes, 
+// which correspond to each image
+// To access the array with the dimensions, use cluster_images[i]
+
+extern vector<int*> initial_centroids;
+extern vector<int*> current_centroids;
+extern vector<int*> previous_centroid;
+// in cluster images, I save number_of_images images (is a d-dimensional array)
+extern int** cluster_images;
+
+vector<pair<int*, vector<int> > > kmeansPP(int, uint32_t, uint64_t);
+vector<double> silhouette(vector<pair<int *, vector<int>>>, u_int64_t);
+unsigned int manhattanDistance(int*, int*, uint64_t);
---
+++ src/d.Cluster/modules/manhattan-distance.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <stdint.h>
+
+unsigned int manhattanDistance(int* x, int* y, uint64_t n) {
+	unsigned int sum =0;
+    for (uint64_t i = 0; i < n; i++) {
+        sum += abs(x[i] - y[i]); 
+    }
+    return sum;
+}
